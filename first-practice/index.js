5232c16e04823780c40dd05fe3e0197294f24b7c
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-03-09 09:43:26 +0600

    selecting multiple element using jquery

--- first-practice/index.js
+++ first-practice/index.js
@@ -8,3 +8,14 @@ document.querySelector('h1').innerText = 'Hello Ammu';
 // jquery code 
 
 $('h1').text('Hi ammu')
+
+// select a element using jquery
+
+$("p").text('Hello farzana sultana your son is now a PhD holder')
+
+$("#p1").text("Hello dad I earned my PhD from Caltech")
+
+
+// selecting multiple element
+$('h3,h4').text("once we were a heading with different text but now we all changed")
+
