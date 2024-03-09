f6a2f9426f22b67ccff6a0ba1fe4fadf28d75807
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-03-09 10:34:41 +0600

    tried out Jquery

--- first-practice/index.js
+++ first-practice/index.js
@@ -1,7 +1,7 @@
 // JS code 
 
 
-document.querySelector('h1').innerText = 'Hello Ammu';
+// document.querySelector('h1').innerText = 'Hello Ammu';
 
 
 
@@ -9,7 +9,7 @@ document.querySelector('h1').innerText = 'Hello Ammu';
 
 // selecting a single element
 
-$('h1').text('Hi ammu')
+// $('h1').text('Hi ammu')
 
 // select a element using jquery
 
@@ -31,3 +31,16 @@ $(".large-heading").html("<i><b>Hi ammu dekho ami bold </b></i>")
 $("#container-element").html(
    " <h4>Sakib Ahammed</h4><p>lorem ipsum dolor sit amet i have no idea what ther heck is this thing ever work .. I dont wanna know</p><p>This is a card element based on nothing and i am really tired and u wanna know how this thing ever work </p>"
 )
+//append and prepend in Jquery
+
+// append (add or update a text using jquery as both text mathod and html mathod not gonna update the text , It simply change the whole part)
+
+
+
+// append (add something at the end part of the elemnent and its quite good to go)
+
+$("#name").append(' (MBBS) , Dhaka')
+
+// Prepend a thing ( add something at the start of the element just like append but its for front side)
+
+$("#name").prepend("Dr. ")
