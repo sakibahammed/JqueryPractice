c59f4d0dbc8add257b58f914404834330b3c6ec1
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-03-09 12:53:15 +0600

    manipulating attribute in js and jquery both

---
+++ first-practice/atributeManipulation.js
@@ -0,0 +1,31 @@
+// attributeManipulation using js
+
+// let attribute = document.querySelector('a').getAttribute('href');
+// let fb= `https://www.facebook.com/`
+// attribute = fb
+// console.log(attribute);
+
+
+//get from jquery
+
+let atr = $('a').attr('href')
+console.log(atr);
+
+
+//remove an attribute 
+
+let removedAtr = $('a').removeAttr("href")
+
+$('a').attr('href')
+
+
+// set attribute
+
+//jquery
+
+$('a').attr('href','https://www.facebook.com/')
+
+
+//js
+
+document.querySelector('a').setAttribute('href','https://www.youtube.com/watch?v=eosp4QAbu-U&list=PLgH5QX0i9K3pSJG9Hwjnykd0hLGEsW4DB&index=4');
