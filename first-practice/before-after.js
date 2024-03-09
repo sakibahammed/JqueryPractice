44f2d38e02ab426eb3e3850b7f529b88b4034459
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-03-09 10:53:33 +0600

    before after mathod in jquery

--- first-practice/before-after.js
+++ first-practice/before-after.js
@@ -1,3 +1,8 @@
 const text1 = $("<p></p>").text("This is ground 0 from jquery")
+//before jquery ( create a new element or another or like a new thing before the text)
 
 $('h1').before(text1)
+
+const newTextforAfter = $("<h1></h1>").text('This is from ground2')
+
+$('h1').after(newTextforAfter)
