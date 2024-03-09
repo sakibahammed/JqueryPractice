a22f5ab82a973fa39597536721c50997311299a6
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-03-09 09:59:12 +0600

    tried out .html thing

--- first-practice/index.js
+++ first-practice/index.js
@@ -19,3 +19,7 @@ $("#p1").text("Hello dad I earned my PhD from Caltech")
 // selecting multiple element
 $('h3,h4').text("once we were a heading with different text but now we all changed")
 
+
+// using html Manipulation.
+
+$(".large-heading").html("<i><b>Hi ammu dekho ami bold </b></i>")
