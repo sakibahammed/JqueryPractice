349b42d9ef3cbe0fddf850778f6f0d73c3f238f7
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-03-09 10:21:19 +0600

    creating mulple line using Jquery though i have know a lot of things

--- first-practice/index.js
+++ first-practice/index.js
@@ -7,6 +7,8 @@ document.querySelector('h1').innerText = 'Hello Ammu';
 
 // jquery code 
 
+// selecting a single element
+
 $('h1').text('Hi ammu')
 
 // select a element using jquery
@@ -23,3 +25,9 @@ $('h3,h4').text("once we were a heading with different text but now we all chang
 // using html Manipulation.
 
 $(".large-heading").html("<i><b>Hi ammu dekho ami bold </b></i>")
+
+// creating a card element using jquery
+
+$("#container-element").html(
+   " <h4>Sakib Ahammed</h4><p>lorem ipsum dolor sit amet i have no idea what ther heck is this thing ever work .. I dont wanna know</p><p>This is a card element based on nothing and i am really tired and u wanna know how this thing ever work </p>"
+)
